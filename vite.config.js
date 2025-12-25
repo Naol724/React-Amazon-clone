16e7d04533b607a2a48b9702cc082c59ac69585f
Naol724 <naolgonfa449@gmail.com>
2025-12-25 18:00:41 +0300

    Fix Vite base path for Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production'
-    ? '/React-Ecommerce-App/'
-    : '/',
+  base: '/', // <- always use root for all
 })
